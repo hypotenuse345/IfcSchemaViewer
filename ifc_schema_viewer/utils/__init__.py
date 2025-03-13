378727f57ffa2fdd460d3b3e48bb340123cb7755
panzy <panzeyu@sjtu.edu.cn>
2025-03-13 23:00:46 +0800

    refactor: add IfcConceptRenderer import to utilities module

--- ifc_schema_viewer/utils/__init__.py
+++ ifc_schema_viewer/utils/__init__.py
@@ -1,2 +1,3 @@
 from .echarts import EchartsUtility
 from .graph_algo import GraphAlgoUtility
+from .ifc_schema import IfcConceptRenderer
---
+++ ifc_schema_viewer/utils/ifc_schema.py
@@ -0,0 +1,426 @@
+import rdflib
+import streamlit as st
+from streamlit_extras.markdownlit import mdlit
+
+from pydantic import BaseModel, PrivateAttr, Field
+from typing import List, Optional, Any, Dict, Annotated, Type
+
+ONT = rdflib.Namespace("http://www.semantic.org/zeyupan/ontologies/CoALA4IFC_Schema_Ont#")
+
+class ConceptInfo(BaseModel):
+    iri: Annotated[str, Field(description="The IRI of the concept")]
+    _express_type: str = PrivateAttr("")
+    
+    @property
+    def express_type(self):
+        return self._express_type
+    
+    rdf_graph: Any = Field(description="The RDF graph containing the concept information")
+    
+    def model_post_init(self, __context):
+        if not isinstance(self.rdf_graph, rdflib.Graph):
+            raise ValueError("rdf_graph must be an instance of rdflib.Graph")
+
+    @property
+    def namespace_manager(self):
+        return self.rdf_graph.namespace_manager
+    
+    @property
+    def label(self):
+        return rdflib.URIRef(self.iri).fragment
+    
+    def display(self, container):
+        raise NotImplementedError("Subclasses must implement the display method")
+
+class EnumInfo(ConceptInfo):
+    _members: List[Dict[str, str]] = PrivateAttr(default_factory=list)
+    @property
+    def members(self):
+        return self._members
+    
+    _express_type: str = PrivateAttr("express:Enum")
+    
+    def model_post_init(self, __context):
+        super().model_post_init(__context)
+        
+        results = self.rdf_graph.query(
+            f"""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT DISTINCT ?member_name ?member_description
+            WHERE {{
+                <{self.iri}> <{ONT["hasValue"]}> ?member .
+                ?member a <{ONT["EnumValue"]}>;
+                    <{ONT["name"]}> ?member_name;
+                    <{ONT["description"]}> ?member_description.
+            }}
+            """
+        )
+        for result_row in results:
+            self.members.append({
+                "enum value": result_row.member_name,
+                "description": result_row.member_description
+            })
+    
+    def display(self, container):
+        with container:
+            st.dataframe(self.members, hide_index=True, use_container_width=True)
+            
+class PropertyEnumInfo(EnumInfo):
+    _express_type: str = PrivateAttr("express:PropertyEnumeration")
+
+class SelectInfo(ConceptInfo):
+    _members: List[Dict[str, str]] = PrivateAttr(default_factory=list)
+    @property
+    def members(self):
+        return self._members
+
+    _express_type: str = PrivateAttr("express:Select")
+
+    def model_post_init(self, __context):
+        super().model_post_init(__context)
+
+        results = self.rdf_graph.query(
+            f"""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT DISTINCT ?member ?member_name ?express_type
+            WHERE {{
+                <{self.iri}> <{ONT["hasValue"]}> ?member .
+                ?member <{ONT["name"]}> ?member_name;
+                    a ?express_type.
+                FILTER (STRSTARTS(str(?express_type), "{ONT}"))
+            }}
+            """
+        )
+        for result_row in results:
+            self.members.append({
+                "select value": result_row.member_name,
+                "express type": result_row.express_type.n3(self.namespace_manager),
+                "iri": result_row.member
+            })
+    def display(self, container):
+        with container:
+            st.write(f"#### *Select Values*")
+            selected = st.dataframe(
+                self.members, hide_index=True, 
+                use_container_width=True, selection_mode="single-row",
+                on_select="rerun", column_order=["select value", "express type"])
+        if selected["selection"]["rows"]:
+            selected_index = selected["selection"]["rows"][0]
+            member = self.members[selected_index]
+            IfcConceptRenderer.display_selected_individual_info(member["express type"], member["iri"], self.rdf_graph)
+
+class EntityInfo(ConceptInfo):
+    _express_type: str = PrivateAttr("express:Entity")
+    _super_entities : List[Dict[str, str]] = PrivateAttr(default_factory=list)
+    _sub_entities : List[Dict[str, str]] = PrivateAttr(default_factory=list)
+    _direct_attributes : List[Dict[str, str]] = PrivateAttr(default_factory=list)
+    _inverse_attributes : List[Dict[str, str]] = PrivateAttr(default_factory=list)
+    @property
+    def super_entities(self):
+        return self._super_entities
+    @property
+    def sub_entities(self):
+        return self._sub_entities
+    @property
+    def direct_attributes(self):
+        return self._direct_attributes
+    @property
+    def inverse_attributes(self):
+        return self._inverse_attributes
+
+    def model_post_init(self, __context):
+        super().model_post_init(__context)
+
+        # 父实体
+        results = self.rdf_graph.query(
+            f"""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT DISTINCT ?super_entity ?super_entity_name ?definitions
+            WHERE {{
+                <{self.iri}> <{ONT["subClassOf"]}>+ ?super_entity .
+                ?super_entity <{ONT["name"]}> ?super_entity_name;
+                    <{ONT["definitions"]}> ?definitions.
+            }}"""
+        )
+        for result_row in results:
+            self.super_entities.append({
+                "type": "express:Entity",
+                "name": result_row.super_entity_name,
+                "iri": result_row.super_entity,
+                "definitions": result_row.definitions
+            })
+            
+        # 子实体
+        results = self.rdf_graph.query(
+            f"""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT DISTINCT ?sub_entity ?sub_entity_name ?definitions
+            WHERE {{
+                ?sub_entity <{ONT["subClassOf"]}>+ <{self.iri}> .
+                ?sub_entity <{ONT["name"]}> ?sub_entity_name;
+                    <{ONT["definitions"]}> ?definitions.
+            }}"""
+        )
+        for result_row in results:
+            self.sub_entities.append({
+                "type": "express:Entity",
+                "name": result_row.sub_entity_name,
+                "iri": result_row.sub_entity,
+                "definitions": result_row.definitions
+            })
+        # 直接属性
+        results = self.rdf_graph.query(
+            f"""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT DISTINCT ?attr_name ?description ?optional ?direct_attr_num ?cardinality ?attrRange ?express_type
+            WHERE {{
+                <{self.iri}> <{ONT["hasDirectAttribute"]}> ?attr .
+                ?attr <{ONT["name"]}> ?attr_name;
+                    <{ONT["is_optional"]}> ?optional;
+                    <{ONT["description"]}> ?description;
+                    <{ONT["direct_attr_num"]}> ?direct_attr_num;
+                    <{ONT["cardinality"]}> ?cardinality;
+                    <{ONT["attrRange"]}> ?attrRange.
+                ?attrRange a ?express_type.
+                FILTER (STRSTARTS(str(?express_type), "{ONT}"))
+            }}"""
+        )
+        for result_row in results:
+            self.direct_attributes.append({
+                "#": int(result_row.direct_attr_num),
+                "name": result_row.attr_name,
+                "optional": "T" if result_row.optional else "F",
+                "cardinality": result_row.cardinality,
+                "range": result_row.attrRange.fragment,
+                "express type": result_row.express_type.n3(self.rdf_graph.namespace_manager),
+                "attr datatype": result_row.attrRange,
+                "description": result_row.description,
+            })
+        self.direct_attributes.sort(key=lambda x: x["#"])
+        
+        # 间接属性
+        results = self.rdf_graph.query(
+            f"""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT DISTINCT ?attr_name ?description ?optional ?direct_attr_num ?cardinality ?attrRange ?express_type
+            WHERE {{
+                <{self.iri}> <{ONT["hasInverseAttribute"]}> ?attr .
+                ?attr <{ONT["name"]}> ?attr_name;
+                    <{ONT["is_optional"]}> ?optional;
+                    <{ONT["description"]}> ?description;
+                    <{ONT["cardinality"]}> ?cardinality;
+                    <{ONT["attrRange"]}> ?attrRange.
+                ?attrRange a ?express_type.
+                FILTER (STRSTARTS(str(?express_type), "{ONT}"))
+            }}"""
+        )
+        
+        for result_row in results:
+            self.inverse_attributes.append({
+                "#": "",
+                "name": result_row.attr_name,
+                "optional": "T" if result_row.optional else "F",
+                "cardinality": result_row.cardinality,
+                "range": result_row.attrRange.fragment,
+                "express type": result_row.express_type.n3(self.rdf_graph.namespace_manager),
+                "attr datatype": result_row.attrRange,
+                "description": result_row.description,
+            })
+    
+    def _display_super_entities(self, container):
+        with container:
+            st.write(f"#### *Super Entities*")
+            selected_index = None
+            if self.super_entities:
+                selected = st.dataframe(
+                    self.super_entities, hide_index=True, 
+                    use_container_width=True, selection_mode="single-row",
+                    on_select="rerun", column_order=["type", "name", "definitions"])
+                if selected["selection"]["rows"]:
+                    selected_index = selected["selection"]["rows"][0]
+            else:
+                st.write("None")
+        if selected_index is not None:
+            super_entity = self.super_entities[selected_index]
+            IfcConceptRenderer.display_selected_individual_info("express:Entity", super_entity["iri"], self.rdf_graph)
+    
+    def _display_sub_entities(self, container):
+        with container:
+            st.write(f"#### *Sub Entities*")
+            selected_index = None
+            if self.sub_entities:
+                selected = st.dataframe(
+                    self.sub_entities, hide_index=True, 
+                    use_container_width=True, selection_mode="single-row",
+                    on_select="rerun", column_order=["type", "name", "definitions"])
+                if selected["selection"]["rows"]:
+                    selected_index = selected["selection"]["rows"][0]
+            else:
+                st.write("None")
+        if selected_index is not None:
+            sub_entity = self.sub_entities[selected_index]
+            IfcConceptRenderer.display_selected_individual_info("express:Entity", sub_entity["iri"], self.rdf_graph)
+    
+    def _display_direct_attributes(self, container):
+        with container:
+            st.write(f"#### *Direct Attributes*")
+            selected = st.dataframe(
+                self.direct_attributes, hide_index=True, 
+                use_container_width=True,
+                column_order=["#", "name", "optional", "cardinality", "range", "express type", "description"],
+                selection_mode="single-row", on_select="rerun")
+        if selected["selection"]["rows"]:
+            direct_attr_selected_index = selected["selection"]["rows"][0]
+            selected = self.direct_attributes[direct_attr_selected_index]
+            IfcConceptRenderer.display_selected_individual_info(selected["express type"], selected["attr datatype"], self.rdf_graph)
+    
+    def _display_inverse_attributes(self, container):
+        with container:
+            st.write(f"#### *Inverse Attributes*")
+            selected = st.dataframe(
+                self.inverse_attributes, hide_index=True, 
+                use_container_width=True,
+                column_order=["#", "name", "optional", "cardinality", "range", "express type", "description"],
+                selection_mode="single-row", on_select="rerun")
+        if selected["selection"]["rows"]:
+            inverse_attr_selected_index = selected["selection"]["rows"][0]
+            selected = self.inverse_attributes[inverse_attr_selected_index]
+            IfcConceptRenderer.display_selected_individual_info(selected["express type"], selected["attr datatype"], self.rdf_graph)
+    
+    def display(self, container):
+        self._display_super_entities(container)
+        self._display_sub_entities(container)
+        self._display_direct_attributes(container)
+        self._display_inverse_attributes(container)
+
+class PsetInfo(ConceptInfo):
+    _express_type: str = PrivateAttr("express:PropertySetTemplate")
+    _props: List[Dict[str, str]] = PrivateAttr(default_factory=list)
+    @property
+    def props(self):
+        return self._props
+    
+    def model_post_init(self, __context):
+        super().model_post_init(__context)
+        
+        results = self.rdf_graph.query(
+            f"""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT DISTINCT ?prop_name ?description ?data_type ?property_type ?dataType ?express_type
+            WHERE {{
+                <{self.iri}> <{ONT["hasPropTemplate"]}> ?prop .
+                ?prop <{ONT["name"]}> ?prop_name;
+                    <{ONT["data_type"]}> ?data_type;
+                    <{ONT["description"]}> ?description;
+                    <{ONT["property_type"]}> ?property_type;
+                    <{ONT["dataType"]}> ?dataType.
+                ?dataType a ?express_type.
+                FILTER (STRSTARTS(str(?express_type), "{ONT}"))
+            }}"""
+        )
+        for result_row in results:
+            self.props.append({
+                "property": result_row.prop_name,
+                "property_type": result_row.property_type,
+                "data_type": result_row.data_type,
+                "dataType": result_row.dataType,
+                "express type": result_row.express_type.n3(self.rdf_graph.namespace_manager),
+                "description": result_row.description,
+            })
+    
+    def display(self, container):
+        with container:
+            st.write(f"#### *Properties*")
+            selected = st.dataframe(
+                self.props, hide_index=True, 
+                use_container_width=True,
+                column_order=["property", "property_type", "data_type", "express type", "description"],
+                on_select="rerun", selection_mode="single-row"
+            )
+        if selected["selection"]["rows"]:
+            selected_index = selected["selection"]["rows"][0]
+            prop = self.props[selected_index]
+            IfcConceptRenderer.display_selected_individual_info(prop["express type"], prop["dataType"], self.rdf_graph)
+
+class QsetInfo(PsetInfo):
+    _express_type: str = PrivateAttr("express:QuantitySetTemplate")
+    
+class DerivedTypeInfo(ConceptInfo):
+    _express_type: str = PrivateAttr("express:DerivedType")
+    
+    _derived_from: str = PrivateAttr(None)
+    _cardinality: str = PrivateAttr(None)
+    
+    @property
+    def derived_from(self):
+        return self._derived_from
+
+    @property
+    def cardinality(self):
+        return self._cardinality
+
+    def model_post_init(self, __context):
+        super().model_post_init(__context)
+
+        results = self.rdf_graph.query(
+            f"""SELECT ?derived_from ?cardinality
+            WHERE {{
+                <{self.iri}> <{ONT["derivedFrom"]}> ?derived_from;
+                    <{ONT["cardinality"]}> ?cardinality.
+            }}
+            """
+        )
+        for result_row in results:
+            derived_from = result_row.derived_from
+            cardinality = result_row.cardinality
+            if derived_from:
+                derived_from = derived_from.fragment
+            self._derived_from = derived_from
+            self._cardinality = cardinality
+        
+    def display(self, container):
+        with container:
+            if str(self.cardinality) != "1":
+                st.write(f"{self.cardinality} *{self.derived_from}*")
+            else:
+                st.write(f"*{self.derived_from}*")
+            
+
+concept_info_map: Dict[str, Type[ConceptInfo]] = {
+    "express:Enum": EnumInfo,
+    "express:PropertyEnumeration": PropertyEnumInfo,
+    "express:Select": SelectInfo,
+    "express:Entity": EntityInfo,
+    "express:PropertySetTemplate": PsetInfo,
+    "express:QuantitySetTemplate": QsetInfo,
+    "express:DerivedType": DerivedTypeInfo
+}
+        
+class IfcConceptRenderer:
+    
+    @staticmethod
+    def display_selected_individual_info(express_type, individual_iri, ifc_schema_graph: rdflib.Graph):
+        if express_type not in concept_info_map:
+            return
+        concept_info_class = concept_info_map[express_type]
+        if st.session_state.get("cached_concept_info", None) is None:
+            st.session_state.cached_concept_info = {}
+        if st.session_state.cached_concept_info.get(individual_iri, None) is None:
+            st.session_state.cached_concept_info[individual_iri] = concept_info_class(iri=individual_iri, rdf_graph=ifc_schema_graph)
+        concept_info = st.session_state.cached_concept_info[individual_iri]
+        
+        container = st.expander(label=f"**{concept_info.label}**", expanded=True)
+        with container:
+            st.header(f"{concept_info.label}", divider=True)
+            mdlit(f"@({concept_info.label})(https://ifc43-docs.standards.buildingsmart.org/IFC/RELEASE/IFC4x3/HTML/lexical/{concept_info.label}.htm)")
+        
+        concept_info.display(container)
+        
+            
